ba19c5f70a00752c35569f61c9938fb44eec3f3b
Revi Permana <revipermana231@gmail.com>
2021-01-27 09:47:29 +0700

    day2

--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,23 @@
 <?php
 
+use App\Http\Controllers\CategoryController;
+use App\Http\Controllers\SupplierController;
+use App\Http\Controllers\ProductController;
+use App\Http\Controllers\ExampleController;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [ExampleController::class, 'index']);
+Route::get('/example1', [ExampleController::class, 'example1']);
+
+Route::get('/categories', [CategoryController::class, 'index']);
+Route::get('/categories/add', [CategoryController::class, 'add']);
+Route::get('/categories/{id}/edit', [CategoryController::class, 'edit']);
+
+Route::get('/suppliers', [SupplierController::class, 'index']);
+Route::get('/suppliers/add', [SupplierController::class, 'add']);
+Route::get('/suppliers/{id}/edit', [SupplierController::class, 'edit']);
+
+Route::get('/products', [ProductController::class, 'index']);
+Route::get('/products/add', [ProductController::class, 'add']);
+Route::get('/products/{id}/edit', [ProductController::class, 'edit']);
